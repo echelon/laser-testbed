fe4304e4ff566cf699c55fadc73864be8c4d233f
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-08-08 03:00:47 -0400

    A spiral shape, has blanking now

--- simple/spiral.py
+++ simple/spiral.py
@@ -13,9 +13,7 @@ class SpiralPointStream(object):
 
 		MAXRAD =  32600
 
-		USERAD = MAXRAD * 1.0
-	
-		RESIZE_SPEED_INV  = 200
+		USERAD = MAXRAD / 100
 
 		SPIRAL_DECAY = 10
 
@@ -23,11 +21,22 @@ class SpiralPointStream(object):
 			rad = int(USERAD)
 			j = 0
 			for i in xrange(0, 1000, 1):
-				j += SPIRAL_DECAY
-				i = float(i) / 100 * 2 * math.pi
-				x = int(math.cos(i) * (rad - j))
-				y = int(math.sin(i) * (rad - j)) 
-				yield (x, y, CMAX, CMAX, CMAX) 
+				i = float(i) / 1000 * 2 * math.pi * 14
+				j = i
+
+				# Spirals are of the form:
+				# a*x * trig(x), a is const
+				x = int(j * math.cos(i) * rad)
+				y = int(j * math.sin(i) * rad)
+				yield(x, y, CMAX / 2, CMAX / 2, 0) 
+
+			# Blanking
+			i = float(1000) / 1000 * 2 * math.pi * 14
+			j = i
+			x = int(j * math.cos(i) * rad)
+			y = int(j * math.sin(i) * rad)
+			for i in range(1, 20):
+				yield(x - x*i/20 , y - y*i/20, 0, 0, 0)
 
 	def __init__(self):
 		self.called = False
