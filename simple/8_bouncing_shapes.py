6dd8e55296275c6f660ecb1f0c18f7d10c2ece58
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-08-29 19:37:25 -0400

    Shapes

--- simple/8_bouncing_shapes.py
+++ simple/8_bouncing_shapes.py
@@ -23,17 +23,23 @@ The gui will alter these.
 """
 LASER_POWER_DENOM = 1
 SHOW_BLANKING_PATH = False 
-BLANK_SAMPLE_PTS = 5 
-BALL_SAMPLE_PTS = 60 # XXX: Needs to increase proportionally to radius
+BLANK_SAMPLE_PTS = 12 
+
+# XXX: Needs to increase proportionally to radius
+BALL_SAMPLE_PTS = 20 
+TRIANGLE_EDGE_SAMPLE_PTS = 20
+SQUARE_EDGE_SAMPLE_PTS = 20 
+
 PAUSE_START_PTS = 8 # 8 seems optimum
 PAUSE_END_PTS = 8 # 8 seems optimum
 CIRCLE_ROTATIONS = 1
 BOUNCE_VEL_MIN = 75 
 BOUNCE_VEL_MAX = 500 
-RADIUS_MIN = 500
+
+RADIUS_MIN = 3000
 RADIUS_MAX = 7000
 
-NUM_BALLS = 8  # XXX: STARUP ADJUSTABLE ONLY
+NUM_BALLS = 8 # XXX: STARUP ADJUSTABLE ONLY
 
 class Entity(object):
 	"""
@@ -121,6 +127,117 @@ class Ball(Entity):
 
 		self.drawn = True
 
+
+class Square(Entity):
+	def __init__(self, x = 0, y = 0, r = 0, g = 0, b = 0, radius = 1200):
+		super(Square, self).__init__(x, y, r, g, b)
+		self.radius = radius 
+		self.drawn = False
+
+		self.pauseFirst = True 
+		self.pauseLast = True 
+
+	def produce(self):
+		"""
+		Generate the points of the circle.
+		"""
+		r, g, b = (0, 0, 0)
+
+		"""
+		Figured it out! This is where the "tails" were coming from!
+		We have to blank first with NO color. The lasers turn on before
+		the galvos reach their destination. Duh. I had it figured in 
+		reverse. 
+		"""
+		"""
+		if self.pauseFirst:
+			x = int(math.cos(0) * self.radius) + self.x
+			y = int(math.sin(0) * self.radius) + self.y
+			r = 0 if not self.r else int(CMAX / LASER_POWER_DENOM)
+			g = 0 if not self.g or LASER_POWER_DENOM > 4 else CMAX
+			b = 0 if not self.b else int(CMAX / LASER_POWER_DENOM)
+			self.lastPt = (x, y, 0, 0, 0)
+			for i in xrange(PAUSE_START_PTS):
+				yield self.lastPt
+		"""
+
+		# Generate points
+		ed = self.radius/2
+
+		x1 = self.x + ed
+		y1 = self.y + ed
+		x2 = self.x - ed
+		y2 = self.y + ed
+		x3 = self.x - ed
+		y3 = self.y - ed
+		x4 = self.x + ed
+		y4 = self.y - ed
+	
+		pt1 = {'x': x1, 'y': y1}
+		pt2 = {'x': x2, 'y': y2}
+		pt3 = {'x': x3, 'y': y3}
+		pt4 = {'x': x4, 'y': y4}
+
+		r = 0 if not self.r else int(CMAX / LASER_POWER_DENOM)
+		g = 0 if not self.g or LASER_POWER_DENOM > 4 else CMAX
+		b = 0 if not self.b else int(CMAX / LASER_POWER_DENOM)
+
+		def make_line(pt1, pt2, steps=200):
+			xdiff = pt1['x'] - pt2['x']
+			ydiff = pt1['y'] - pt2['y']
+			line = []
+			for i in xrange(0, steps, 1):
+				j = float(i)/steps
+				x = pt1['x'] - (xdiff * j)
+				y = pt1['y'] - (ydiff * j)
+				line.append((x, y, r, g, b)) # XXX FIX COLORS
+			return line
+
+		for p in make_line(pt1, pt2, SQUARE_EDGE_SAMPLE_PTS):
+			yield p
+		
+		for p in make_line(pt2, pt3, SQUARE_EDGE_SAMPLE_PTS):
+			yield p
+
+		for p in make_line(pt3, pt4, SQUARE_EDGE_SAMPLE_PTS):
+			yield p
+
+		for p in make_line(pt4, pt1, SQUARE_EDGE_SAMPLE_PTS):
+			self.lastPt = p
+			yield p
+
+
+
+		"""
+		for i in xrange(BALL_SAMPLE_PTS):
+			i = 2 * math.pi * float(i) / BALL_SAMPLE_PTS * CIRCLE_ROTATIONS 
+			x = int(math.cos(i) * self.radius) + self.x
+			y = int(math.sin(i) * self.radius) + self.y
+
+			r = 0 if not self.r else int(CMAX / LASER_POWER_DENOM)
+			g = 0 if not self.g or LASER_POWER_DENOM > 4 else CMAX
+			b = 0 if not self.b else int(CMAX / LASER_POWER_DENOM)
+
+			self.lastPt = (x, y, r, g, b)
+			yield self.lastPt
+		"""
+
+
+		"""
+		if self.pauseLast:
+			# XXX: Crude hack for broken edge
+			for i in xrange(PAUSE_END_PTS): 
+				yield (self.firstPt[0], self.firstPt[1], r, g, b)
+
+			for i in xrange(PAUSE_END_PTS):
+				yield self.firstPt
+
+		"""
+		self.drawn = True
+
+
+
+
 class Triangle(Entity):
 	def __init__(self, x = 0, y = 0, r = 0, g = 0, b = 0, radius = 1200):
 		super(Triangle, self).__init__(x, y, r, g, b)
@@ -172,7 +289,11 @@ class Triangle(Entity):
 		pt2 = {'x': x2, 'y': y2}
 		pt3 = {'x': x3, 'y': y3}
 
-		def make_line(pt1, pt2, backward=False, steps = 25):
+		r = 0 if not self.r else int(CMAX / LASER_POWER_DENOM)
+		g = 0 if not self.g or LASER_POWER_DENOM > 4 else CMAX
+		b = 0 if not self.b else int(CMAX / LASER_POWER_DENOM)
+
+		def make_line(pt1, pt2, steps=200):
 			xdiff = pt1['x'] - pt2['x']
 			ydiff = pt1['y'] - pt2['y']
 			line = []
@@ -180,16 +301,16 @@ class Triangle(Entity):
 				j = float(i)/steps
 				x = pt1['x'] - (xdiff * j)
 				y = pt1['y'] - (ydiff * j)
-				line.append((x, y, CMAX/2, CMAX/2, CMAX/2)) # XXX FIX COLORS
+				line.append((x, y, r, g, b)) # XXX FIX COLORS
 			return line
 
-		for p in make_line(pt1, pt2):
+		for p in make_line(pt1, pt2, TRIANGLE_EDGE_SAMPLE_PTS):
 			yield p
 		
-		for p in make_line(pt2, pt3):
+		for p in make_line(pt2, pt3, TRIANGLE_EDGE_SAMPLE_PTS):
 			yield p
 
-		for p in make_line(pt3, pt1):
+		for p in make_line(pt3, pt1, TRIANGLE_EDGE_SAMPLE_PTS):
 			self.lastPt = p
 			yield p
 
@@ -294,10 +415,13 @@ Main Program
 balls = []
 for i in range(NUM_BALLS):
 	b = None
-	if random.randint(0, 1):
+	r = random.randint(0, 2)
+	if r == 0:
 		b = Ball(0, 0, 0, 0, CMAX, random.randint(RADIUS_MIN, RADIUS_MAX))
-	else:
-		b = Triangle(0, 0, 0, 0, CMAX, random.randint(RADIUS_MIN, RADIUS_MAX))
+	elif r == 1:
+		b = Triangle(0, 0, 0, CMAX, 0, random.randint(RADIUS_MIN, RADIUS_MAX))
+	elif r == 2:
+		b = Square(0, 0, CMAX, 0, 0, random.randint(RADIUS_MIN, RADIUS_MAX))
 	balls.append(b)
 
 ps = PointStream()
