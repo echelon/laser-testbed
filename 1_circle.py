ba3afa0122ecbf7dde835a115c7facc4b20e9912
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-09-23 02:11:09 -0400

    rem spaces

--- 1_circle.py
+++ 1_circle.py
@@ -16,17 +16,17 @@ SAMPLE_PTS = 100 # 30 and below very damaging to galvos
 class CirclePointStream(object):
 
 	def produce(self):
-	
+
 		RESIZE_SPEED_INV = 200
-		while True: 
+		while True:
 			rad = int(USERAD)
 			for i in xrange(0, SAMPLE_PTS, 1):
 				i = float(i) / SAMPLE_PTS * 2 * math.pi
 				x = int(math.cos(i) * rad)
-				y = int(math.sin(i) * rad) 
+				y = int(math.sin(i) * rad)
 				yield (x, y, CMAX/LASER_POWER_DENOM,
-							 CMAX/LASER_POWER_DENOM,
-							 CMAX/LASER_POWER_DENOM)
+							CMAX/LASER_POWER_DENOM,
+							CMAX/LASER_POWER_DENOM)
 
 	def __init__(self):
 		self.called = False
