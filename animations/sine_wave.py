f30ad06b72acf9fea79e71f18d8a3f9cba74695b
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-10-14 20:55:53 -0400

    fix

--- animations/sine_wave.py
+++ animations/sine_wave.py
@@ -83,80 +83,18 @@ class SineWave(Shape):
 			i = (percent * periods) + self.sinePos
 			y = int(math.sin(i) * self.sineAmp) + self.y
 
-			# XXX FIX MATHS: Something wrong, but does it matter?
-			r = int(math.floor(self.g - rh*percent))
-			g = self.g
-			b = int(math.floor(self.b - bh + bh*percent))
+			# XXX FIX MATHS: Something wrong, 
+			# but does it matter?
+			r = int(abs(math.floor(self.g -
+						rh*percent)))
+			g = abs(self.g)
+			b = int(abs(math.floor(self.b -
+						bh + bh*percent)))
 
 			s = (x, y, r, g, b)
 			if s[0] == 0 or s[1] == 0:
 				continue # XXX DEBUG
 			yield s
-
-		"""
-		math.sin(i)
-
-		pts = []
-		pts.append({'x': ed, 'y': ed})
-		pts.append({'x': -ed, 'y': ed})
-		pts.append({'x': -ed, 'y': -ed})
-		pts.append({'x': ed, 'y': -ed})
-
-		# Rotate points
-		for p in pts:
-			x = p['x']
-			y = p['y']
-			p['x'] = x*math.cos(self.theta) - y*math.sin(self.theta)
-			p['y'] = y*math.cos(self.theta) + x*math.sin(self.theta)
-
-		# Translate points
-		for pt in pts:
-			pt['x'] += self.x
-			pt['y'] += self.y
-
-		r = 0 if not self.r else int(self.r / LASER_POWER_DENOM)
-		g = 0 if not self.g or LASER_POWER_DENOM > 4 else self.g
-		b = 0 if not self.b else int(self.b / LASER_POWER_DENOM)
-
-		def make_line(pt1, pt2, steps=200):
-			xdiff = pt1['x'] - pt2['x']
-			ydiff = pt1['y'] - pt2['y']
-			line = []
-			for i in xrange(0, steps, 1):
-				j = float(i)/steps
-				x = pt1['x'] - (xdiff * j)
-				y = pt1['y'] - (ydiff * j)
-				line.append((x, y, r, g, b)) # XXX FIX COLORS
-			return line
-
-		# DRAW THE SHAPE
-
-		p = None # Save in scope
-
-		for p in make_line(pts[0], pts[1], SQUARE_EDGE_SAMPLE_PTS):
-			break
-		for i in range(int(round(SQUARE_VERTEX_SAMPLE_PTS/2.0))):
-			yield p
-		for p in make_line(pts[0], pts[1], SQUARE_EDGE_SAMPLE_PTS):
-			yield p
-		for i in range(SQUARE_VERTEX_SAMPLE_PTS):
-			yield p
-		for p in make_line(pts[1], pts[2], SQUARE_EDGE_SAMPLE_PTS):
-			yield p
-		for i in range(SQUARE_VERTEX_SAMPLE_PTS):
-			yield p
-		for p in make_line(pts[2], pts[3], SQUARE_EDGE_SAMPLE_PTS):
-			yield p
-		for i in range(SQUARE_VERTEX_SAMPLE_PTS):
-			yield p
-		for p in make_line(pts[3], pts[0], SQUARE_EDGE_SAMPLE_PTS):
-			self.lastPt = p # KEEP BOTH
-			yield p
-		for i in range(int(round(SQUARE_VERTEX_SAMPLE_PTS/2.0))):
-			self.lastPt = p # KEEP BOTH
-			yield p
-
-		"""
 
 		self.drawn = True
 
